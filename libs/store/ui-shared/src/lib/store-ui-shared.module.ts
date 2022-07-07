46f86c04a8d64b17a7ac33fc42f4f372f2340a08
Katerina Skroumpelou <sk.katherine@gmail.com>
2022-07-07 16:11:32 +0300

    lab 12 - boundaries

--- libs/store/ui-shared/src/lib/store-ui-shared.module.ts
+++ libs/store/ui-shared/src/lib/store-ui-shared.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
-
 @NgModule({
   imports: [CommonModule, MatToolbarModule],
   declarations: [HeaderComponent],

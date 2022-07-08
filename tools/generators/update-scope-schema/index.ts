efc578f53ef3cf2d11cf85940a6091fbb11a8de7
Katerina Skroumpelou <sk.katherine@gmail.com>
2022-07-08 11:35:01 +0300

    lab14-c

--- tools/generators/update-scope-schema/index.ts
+++ tools/generators/update-scope-schema/index.ts
@@ -25,10 +25,10 @@ function getScopes(projectMap: Map<string, ProjectConfiguration>) {
 
 function replaceScopes(content: string, scopes: string[]): string {
   const joinScopes = scopes.map((s) => `'${s}'`).join(' | ');
-  const PATTERN = /interface Schema \{\n.*\n.*\n\}/gm;
+  const PATTERN = /interface UtilLibSchema \{\n.*\n.*\n\}/gm;
   return content.replace(
     PATTERN,
-    `interface Schema {
+    `interface UtilLibSchema {
   name: string;
   directory: ${joinScopes};
 }`

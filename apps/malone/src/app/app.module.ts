fbcb1303593c771798d7f4031ec3203420d885a1
tobitubbie <triplttt@gmail.com>
2022-07-08 11:25:50 +0200

    adds dynamic header

--- apps/malone/src/app/app.module.ts
+++ apps/malone/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import {StoreUiSharedModule} from "@bg-hoard/store/ui-shared";
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+    imports: [BrowserModule, StoreUiSharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })

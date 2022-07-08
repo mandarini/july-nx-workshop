503cd2e2c6e44ed465f9c08cc8408cb5273c0ec7
Katerina Skroumpelou <sk.katherine@gmail.com>
2022-07-08 10:51:37 +0300

    lab13-c

--- tools/generators/util-lib/index.ts
+++ tools/generators/util-lib/index.ts
@@ -1,12 +1,19 @@
 import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
 import { libraryGenerator } from '@nrwl/workspace/generators';
 
-export default async function (tree: Tree, schema: any) {
-  await libraryGenerator(tree, { name: schema.name });
-  console.log('Here is the name I provided: ', schema.name);
+export interface UtilLibSchema {
+  name: string;
+  directory: 'store' | 'api' | 'shared';
+}
+
+export default async function (tree: Tree, schema: UtilLibSchema) {
+  await libraryGenerator(tree, {
+    name: `util-${schema.name}`,
+    directory: schema.directory,
+    tags: `type:util,scope:${schema.directory}`,
+  });
   await formatFiles(tree);
   return () => {
-    console.log('This will not be executed during dry-run');
     installPackagesTask(tree);
   };
 }

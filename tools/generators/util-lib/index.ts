5324d5c23d8d394d163b49030ac7151d978834be
Katerina Skroumpelou <sk.katherine@gmail.com>
2022-07-08 10:29:17 +0300

    lab13-b

--- tools/generators/util-lib/index.ts
+++ tools/generators/util-lib/index.ts
@@ -3,8 +3,10 @@ import { libraryGenerator } from '@nrwl/workspace/generators';
 
 export default async function (tree: Tree, schema: any) {
   await libraryGenerator(tree, { name: schema.name });
+  console.log('Here is the name I provided: ', schema.name);
   await formatFiles(tree);
   return () => {
+    console.log('This will not be executed during dry-run');
     installPackagesTask(tree);
   };
 }
